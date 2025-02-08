6d4f6a94aacc51dd1b6baa3002d2fd5eb4845796
Jesus Martinez <jmch030220@gmail.com>
2025-02-08 01:57:33 -0500

    adding delete

--- api/post/createServices.php
+++ api/post/createServices.php
@@ -13,14 +13,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $sql_doctor = "SELECT id FROM doctores WHERE nombre = '$nombre_doctor'";  
   $result_doctor = $db->query($sql_doctor);  
   $row_doctor = $result_doctor->fetch(PDO::FETCH_ASSOC);  
-  $doctor_id = $row_doctor['id']; // Cambié 'doctor_id' a 'id'  
+  $doctor_id = $row_doctor['id'];   
   
   
   // ----- Obtener mascota_id -----  
   $sql_mascota = "SELECT id FROM mascotas WHERE nombre = '$nombre_mascota'";  
   $result_mascota = $db->query($sql_mascota);  
   $row_mascota = $result_mascota->fetch(PDO::FETCH_ASSOC);  
-  $mascota_id = $row_mascota['id']; // Cambié 'mascota_id' a 'id'  
+  $mascota_id = $row_mascota['id'];   
   
   
   // ----- Insertar en la tabla servicios -----  
